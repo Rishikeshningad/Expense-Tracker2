0e889459d806ab0ab8f5593668b739bb102fc24d
Rishikeshningad <ningadr@gmail.com>
2023-03-06 10:43:19 +0530

    update of api

--- src/components/store/cart-action.js
+++ src/components/store/cart-action.js
@@ -5,7 +5,7 @@ export const fetchCartData = () => {
     return async dispatch => {
         const fetchData = async () => {
             const response = await fetch(
-                'https://expense-6bc0e-default-rtdb.firebaseio.com/cart.json'
+                'https://ex-pense-76286-default-rtdb.firebaseio.com/cart.json'
             );
 
             if (!response.ok) {
@@ -45,7 +45,7 @@ export const sendCartData = (cart) => {
 
       const sendRequest = async () => {
         const response = await fetch(
-          'https://expense-6bc0e-default-rtdb.firebaseio.com/cart.json', 
+          'https://ex-pense-76286-default-rtdb.firebaseio.com/cart.json', 
           {
           method: 'PUT',
           body: JSON.stringify(cart),
--- src/expenses/Expenses.js
+++ src/expenses/Expenses.js
@@ -66,7 +66,7 @@ const ExpenseItems = () => {
 
     if (edit) {
       fetch(
-        `https://expense-6bc0e-default-rtdb.firebaseio.com/expenses/${editId}.json`,
+        `https://ex-pense-76286-default-rtdb.firebaseio.com/expenses/${editId}.json`,
         {
           method: "PUT",
           body: JSON.stringify({
@@ -87,7 +87,7 @@ const ExpenseItems = () => {
       });
     } else {
       fetch(
-        "https://expense-6bc0e-default-rtdb.firebaseio.com/expenses.json",
+        "https://ex-pense-76286-default-rtdb.firebaseio.com/expenses.json",
         {
           method: "POST",
           body: JSON.stringify({
@@ -134,7 +134,7 @@ const ExpenseItems = () => {
 
   useEffect(() => {
     fetch(
-      "https://expense-6bc0e-default-rtdb.firebaseio.com/expenses.json",
+      "https://ex-pense-76286-default-rtdb.firebaseio.com/expenses.json",
       {
         method: "GET",
         headers: {
@@ -182,7 +182,7 @@ const ExpenseItems = () => {
     setExpense(updated);
 
     fetch(
-      `https://expense-6bc0e-default-rtdb.firebaseio.com/expenses/${id}.json`,
+      `https://ex-pense-76286-default-rtdb.firebaseio.com/expenses/${id}.json`,
       {
         method: "DELETE",
         headers: {
@@ -207,7 +207,7 @@ const ExpenseItems = () => {
     setEditId(id);
 
     fetch(
-      `https://expense-6bc0e-default-rtdb.firebaseio.com/expenses/${id}.json`,
+      `https://ex-pense-76286-default-rtdb.firebaseio.com/expenses/${id}.json`,
       { method: "GET" }
     )
       .then((res) => {
